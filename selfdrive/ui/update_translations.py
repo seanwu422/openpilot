d45d9b3db8bb8bb83b79a66859fc4062ffe4850a
Rick Lan <ricklan@gmail.com>
2025-11-04 15:50:23 +0800

    translation

--- selfdrive/ui/update_translations.py
+++ selfdrive/ui/update_translations.py
@@ -11,6 +11,8 @@ POT_FILE = os.path.join(str(TRANSLATIONS_DIR), "app.pot")
 def update_translations():
   files = []
   for root, _, filenames in chain(os.walk(SYSTEM_UI_DIR),
+                                  os.walk(os.path.join(UI_DIR, "..", "..", "dragonpilot", "selfdrive", "ui", "widgets")),
+                                  os.walk(os.path.join(UI_DIR, "..", "..", "dragonpilot", "selfdrive", "ui", "layouts")),
                                   os.walk(os.path.join(UI_DIR, "widgets")),
                                   os.walk(os.path.join(UI_DIR, "layouts")),
                                   os.walk(os.path.join(UI_DIR, "onroad"))):
