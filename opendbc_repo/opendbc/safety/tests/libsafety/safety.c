53e044045565d186d31d9f8b201d0d40ddee25a5
seanwu422 <seanwu422@gmail.com>
2025-12-18 12:39:02 +0000

    Replace opendbc_repo/opendbc with content from commaai/opendbc@d950f67

---
+++ opendbc_repo/opendbc/safety/tests/libsafety/safety.c
@@ -0,0 +1,14 @@
+#include <stdbool.h>
+
+#include "fake_stm.h"
+#include "can.h"
+
+//int safety_tx_hook(CANPacket_t *to_send) { return 1; }
+
+#include "faults.h"
+#include "libc.h"
+#include "safety.h"
+#include "drivers/can_common.h"
+
+// libsafety stuff
+#include "safety_helpers.h"
